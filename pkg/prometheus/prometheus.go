91409d7630d671f4a4f17fb0aac5f6852230ff90
Andrew Seigner <siggy@buoyant.io>
2019-02-09 16:10:16 -0800

    Instrument k8s clients
    The control-plane's clients, specifically the Kubernetes clients, did
    not provide telemetry information.
    
    Introduce a `prometheus.ClientWithTelemetry` wrapper to instrument
    arbitrary clients. Apply this wrapper to Kubernetes clients.
    
    Fixes #2183
    
    Signed-off-by: Andrew Seigner <siggy@buoyant.io>

--- pkg/prometheus/prometheus.go
+++ pkg/prometheus/prometheus.go
@@ -9,17 +9,8 @@ import (
 	"google.golang.org/grpc"
 )
 
-// NewGrpcServer returns a grpc server pre-configured with prometheus interceptors
-func NewGrpcServer() *grpc.Server {
-	server := grpc.NewServer(
-		grpc.UnaryInterceptor(grpc_prometheus.UnaryServerInterceptor),
-		grpc.StreamInterceptor(grpc_prometheus.StreamServerInterceptor),
-	)
-
-	grpc_prometheus.EnableHandlingTimeHistogram()
-	grpc_prometheus.Register(server)
-	return server
-}
+// WrapTransport provides a function for wrapping an http.RoundTripper
+type WrapTransport func(http.RoundTripper) http.RoundTripper
 
 // RequestDurationBucketsSeconds represents latency buckets to record (seconds)
 var RequestDurationBucketsSeconds = append(append(append(append(
@@ -37,37 +28,102 @@ var ResponseSizeBuckets = append(append(append(append(
 	prometheus.LinearBuckets(1000000, 1000000, 5)...),
 )
 
-// WithTelemetry instruments the HTTP server with prometheus
-func WithTelemetry(handler http.Handler) http.HandlerFunc {
-	counter := prometheus.NewCounterVec(
+var (
+	// server metrics
+	counter = prometheus.NewCounterVec(
 		prometheus.CounterOpts{
 			Name: "http_requests_total",
 			Help: "A counter for requests to the wrapped handler.",
 		},
-		[]string{"code"},
+		[]string{"code", "method"},
 	)
 
-	duration := prometheus.NewHistogramVec(
+	duration = prometheus.NewHistogramVec(
 		prometheus.HistogramOpts{
 			Name:    "http_request_duration_seconds",
 			Help:    "A histogram of latencies for requests in seconds.",
 			Buckets: RequestDurationBucketsSeconds,
 		},
-		[]string{"code"},
+		[]string{"code", "method"},
 	)
 
-	responseSize := prometheus.NewHistogramVec(
+	responseSize = prometheus.NewHistogramVec(
 		prometheus.HistogramOpts{
 			Name:    "http_response_size_bytes",
 			Help:    "A histogram of response sizes for requests.",
 			Buckets: ResponseSizeBuckets,
 		},
-		[]string{},
+		[]string{"code", "method"},
 	)
 
-	prometheus.MustRegister(counter, duration, responseSize)
+	// client metrics
+	clientCounter = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "client_api_requests_total",
+			Help: "A counter for requests from the wrapped client.",
+		},
+		[]string{"client", "code", "method"},
+	)
 
+	clientDur = prometheus.NewHistogramVec(
+		prometheus.HistogramOpts{
+			Name:    "client_request_duration_seconds",
+			Help:    "A histogram of request latencies.",
+			Buckets: RequestDurationBucketsSeconds,
+		},
+		[]string{"client", "code", "method"},
+	)
+
+	clientInFlight = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "client_in_flight_requests",
+			Help: "A gauge of in-flight requests for the wrapped client.",
+		},
+		[]string{"client"},
+	)
+)
+
+func init() {
+	prometheus.MustRegister(
+		counter, duration, responseSize, // server metrics
+		clientCounter, clientDur, clientInFlight, // client metrics
+	)
+}
+
+// NewGrpcServer returns a grpc server pre-configured with prometheus interceptors
+func NewGrpcServer() *grpc.Server {
+	server := grpc.NewServer(
+		grpc.UnaryInterceptor(grpc_prometheus.UnaryServerInterceptor),
+		grpc.StreamInterceptor(grpc_prometheus.StreamServerInterceptor),
+	)
+
+	grpc_prometheus.EnableHandlingTimeHistogram()
+	grpc_prometheus.Register(server)
+	return server
+}
+
+// WithTelemetry instruments the HTTP server with prometheus
+func WithTelemetry(handler http.Handler) http.HandlerFunc {
 	return promhttp.InstrumentHandlerDuration(duration,
 		promhttp.InstrumentHandlerResponseSize(responseSize,
 			promhttp.InstrumentHandlerCounter(counter, handler)))
+}
+
+// ClientWithTelemetry instruments the HTTP client with prometheus
+func ClientWithTelemetry(name string, wt WrapTransport) WrapTransport {
+	dur := clientDur.MustCurryWith(prometheus.Labels{"client": name})
+	count := clientCounter.MustCurryWith(prometheus.Labels{"client": name})
+	flight := clientInFlight.With(prometheus.Labels{"client": name})
+
+	return func(rt http.RoundTripper) http.RoundTripper {
+		if wt != nil {
+			rt = wt(rt)
+		}
+
+		return promhttp.InstrumentRoundTripperInFlight(flight,
+			promhttp.InstrumentRoundTripperCounter(count,
+				promhttp.InstrumentRoundTripperDuration(dur, rt),
+			),
+		)
+	}
 }
